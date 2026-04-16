b0859462161fae8ca8394fde277d1d3ce273f350
Pavel Sborshchikov <sborshikow@gmail.com>
2026-04-16 05:55:26 +0300

    keep NativeEventBuffer pointers

--- Runtime/EventsData.cs
+++ Runtime/EventsData.cs
@@ -1,19 +1,27 @@
 ﻿using System;
 using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
 
 namespace ED.DOTS.EntitiesEvents
 {
     /// <summary>
     /// Manages double-buffered event storage for type <typeparamref name="T"/>.
-    /// Internally owns two <see cref="NativeEventBuffer{T}"/> instances and swaps their roles on <see cref="Update"/>.
+    /// Internally owns two <see cref="NativeEventBuffer{T}"/> instances allocated on the heap
+    /// and swaps their roles on <see cref="Update"/>.
     /// </summary>
     /// <typeparam name="T">Unmanaged event type.</typeparam>
-    internal struct EventsData<T> : IDisposable
+    internal unsafe struct EventsData<T> : IDisposable
         where T : unmanaged
     {
-        private NativeEventBuffer<T> _buffer1;
-        private NativeEventBuffer<T> _buffer2;
+        [NativeDisableUnsafePtrRestriction]
+        private NativeEventBuffer<T>* _buffer1;
+
+        [NativeDisableUnsafePtrRestriction]
+        private NativeEventBuffer<T>* _buffer2;
+
         private bool _state; // true: buffer2 is write, buffer1 is read; false: buffer1 is write, buffer2 is read
+
+        private readonly Allocator _allocator;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="EventsData{T}"/> struct with the specified capacity and allocator.
@@ -22,8 +30,29 @@ namespace ED.DOTS.EntitiesEvents
         /// <param name="allocator">Allocator to use for all internal allocations.</param>
         public EventsData(int initialCapacity, Allocator allocator)
         {
-            _buffer1 = new NativeEventBuffer<T>(initialCapacity, allocator);
-            _buffer2 = new NativeEventBuffer<T>(initialCapacity, allocator);
+#if ENABLE_UNITY_COLLECTIONS_CHECKS
+            if (allocator <= Allocator.None)
+                throw new ArgumentException("Allocator must be Temp, TempJob, Persistent or registered custom allocator", nameof(allocator));
+            if (initialCapacity < 0)
+                throw new ArgumentOutOfRangeException(nameof(initialCapacity), "InitialCapacity must be >= 0");
+#endif
+
+            _allocator = allocator;
+
+            // Allocate and initialize buffer1
+            var size = UnsafeUtility.SizeOf<NativeEventBuffer<T>>();
+            var alignment = UnsafeUtility.AlignOf<NativeEventBuffer<T>>();
+            _buffer1 = (NativeEventBuffer<T>*)UnsafeUtility.MallocTracked(size, alignment, allocator, 1);
+            UnsafeUtility.MemClear(_buffer1, size);
+            var temp1 = new NativeEventBuffer<T>(initialCapacity, allocator);
+            UnsafeUtility.CopyStructureToPtr(ref temp1, _buffer1);
+
+            // Allocate and initialize buffer2
+            _buffer2 = (NativeEventBuffer<T>*)UnsafeUtility.MallocTracked(size, alignment, allocator, 1);
+            UnsafeUtility.MemClear(_buffer2, size);
+            var temp2 = new NativeEventBuffer<T>(initialCapacity, allocator);
+            UnsafeUtility.CopyStructureToPtr(ref temp2, _buffer2);
+
             _state = false; // initially buffer1 is write, buffer2 is read
         }
 
@@ -37,27 +66,27 @@ namespace ED.DOTS.EntitiesEvents
             if (_state)
             {
                 // Now buffer2 is write, buffer1 is read
-                _buffer2.Clear();
+                _buffer2->Clear();
             }
             else
             {
                 // Now buffer1 is write, buffer2 is read
-                _buffer1.Clear();
+                _buffer1->Clear();
             }
         }
 
         /// <summary>
-        /// Returns the buffer currently designated for writing.
+        /// Returns a reference to the buffer currently designated for writing.
         /// </summary>
-        public NativeEventBuffer<T> GetWriteBuffer()
+        public NativeEventBuffer<T>* GetWriteBuffer()
         {
             return _state ? _buffer2 : _buffer1;
         }
 
         /// <summary>
-        /// Returns the buffer currently designated for reading.
+        /// Returns a reference to the buffer currently designated for reading.
         /// </summary>
-        public NativeEventBuffer<T> GetReadBuffer()
+        public NativeEventBuffer<T>* GetReadBuffer()
         {
             return _state ? _buffer1 : _buffer2;
         }
@@ -68,17 +97,28 @@ namespace ED.DOTS.EntitiesEvents
         /// <param name="capacity">Minimum capacity required.</param>
         public void EnsureCapacity(int capacity)
         {
-            _buffer1.EnsureCapacity(capacity);
-            _buffer2.EnsureCapacity(capacity);
+            _buffer1->EnsureCapacity(capacity);
+            _buffer2->EnsureCapacity(capacity);
         }
 
         /// <summary>
-        /// Disposes both internal buffers.
+        /// Disposes both internal buffers and frees allocated memory.
         /// </summary>
         public void Dispose()
         {
-            _buffer1.Dispose();
-            _buffer2.Dispose();
+            if (_buffer1 != null)
+            {
+                _buffer1->Dispose();
+                UnsafeUtility.FreeTracked(_buffer1, _allocator);
+                _buffer1 = null;
+            }
+
+            if (_buffer2 != null)
+            {
+                _buffer2->Dispose();
+                UnsafeUtility.FreeTracked(_buffer2, _allocator);
+                _buffer2 = null;
+            }
         }
     }
 }
